438fa6e24030c1c301219a61d6493e076d7c0c0c
andresporras3423 <oscarrussi@outlook.com>
2021-11-20 22:19:47 -0500

    add controller for contacts views

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,5 @@
+class ContactsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
   devise_for :users
+  resources :contacts, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 end
